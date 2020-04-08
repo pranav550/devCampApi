0c9364795a7880b1a42e7f76dd0f2ea328ab2629
pranav550 <pranavverma550>
2020-04-08 13:06:11 +0530

    create route file

--- server.js
+++ server.js
@@ -1,32 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+// routes
+const bootcamps = require("./routes/bootcamp");
+
 
 dotenv.config({path:'./config/config.env'})
 
 const app = express()
-// app.get('/api/v1/bootcamps', (req, res)=>{
-//     res.status(200).json({sucess:true, data:{id:1, name:"pranav"}})
-// })
 
-app.get('/api/v1/bootcamps', (req, res)=>{
-    res.status(200).json({success:true, msg:'Show All bootcamps'})
-})
+app.use('/api/v1/bootcamps', bootcamps )
 
-app.get('/api/v1/bootcamps/:id', (req, res)=>{
-    res.status(200).json({success:true, msg:`Single Bootcamp ${req.params.id}`})
-})
-
-app.post('/api/v1/bootcamps', (req, res)=>{
-    res.status(200).json({success:true, msg:'Create Bootcamp'})
-})
-
-app.put('/api/v1/bootcamps/:id', (req, res)=>{
-    res.status(200).json({success:true, msg:`Update Bootcamp ${req.params.id}`})
-})
-
-app.delete('/api/v1/bootcamps/:id', (req, res)=>{
-    res.status(200).json({success:true, msg:`Delete Bootcamp ${req.params.id}`})
-})
 
 const PORT= process.env.PORT || 5000;
 

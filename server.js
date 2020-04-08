540cdde3b7f1d579885c3fb7f48e6cf025e9ad2a
pranav550 <pranavverma550>
2020-04-08 12:14:35 +0530

    create route

--- server.js
+++ server.js
@@ -4,6 +4,29 @@ const dotenv = require("dotenv");
 dotenv.config({path:'./config/config.env'})
 
 const app = express()
+// app.get('/api/v1/bootcamps', (req, res)=>{
+//     res.status(200).json({sucess:true, data:{id:1, name:"pranav"}})
+// })
+
+app.get('/api/v1/bootcamps', (req, res)=>{
+    res.status(200).json({success:true, msg:'Show All bootcamps'})
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res)=>{
+    res.status(200).json({success:true, msg:`Single Bootcamp ${req.params.id}`})
+})
+
+app.post('/api/v1/bootcamps', (req, res)=>{
+    res.status(200).json({success:true, msg:'Create Bootcamp'})
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res)=>{
+    res.status(200).json({success:true, msg:`Update Bootcamp ${req.params.id}`})
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res)=>{
+    res.status(200).json({success:true, msg:`Delete Bootcamp ${req.params.id}`})
+})
 
 const PORT= process.env.PORT || 5000;
 

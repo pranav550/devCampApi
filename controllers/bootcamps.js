269fcf0cba4443bfb89b871faba4538f3390a0c9
pranav550 <pranavverma550>
2020-04-11 09:29:01 +0530

    seeder filter

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,13 +1,21 @@
 const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require("../models/Bootcamp");
+const geocoder = require('../utils/geocoder');
 const asyncHandler = require("../middleware/async");
 // @desc = Get all bootcamp
 // @route = GET /api/v1/bootcamps
 // @access = Public
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
+        console.log(req.query)
+        let query;
+        let queryStr = JSON.stringify(req.query)
+        console.log(queryStr)
+        queryStr=queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+        console.log(queryStr)
+        query = Bootcamp.find(JSON.parse(queryStr));
 
-        const bootcamps = await Bootcamp.find()
+        const bootcamps = await query
         res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
     })
 
@@ -70,4 +78,36 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
         }
         res.status(200).json({ success: true, data: {} })
    
+})
+
+
+// @desc = GET bootcamps with in a radius
+// @route = GET /api/v1/bootcamps/:zipcode/:distance
+// @access = private
+
+exports.getBootcampInRadius = asyncHandler(async (req, res, next) => {
+    
+const {zipcode, distance}= req.params;
+
+// get lat/lng from geocoder
+const loc = await geocoder.geocode(zipcode);
+console.log(loc,"cccccc")
+const lat = loc[0].latitude
+const lng = loc[0].longitude
+
+// cal radius using radian
+// divide distance by radius of earth
+// earth radius = 3963 mi/ 6378 km 
+
+const radius= distance/3963;
+
+const bootcamps = await Bootcamp.find({
+    location:{$geoWithin:{$centerSphere:[[lng, lat], radius]}}
+})
+res.status(200).json({
+    success:true,
+    count: bootcamps.length,
+    data:bootcamps
+})
+
 })
---
+++ seeder.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// load en vars
+dotenv.config({path:'./config/config.env'});
+
+//load models
+const Bootcamp = require('./models/Bootcamp');
+
+// connect to db
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useUnifiedTopology: true
+})
+
+//Read Json File
+const bootcamps = JSON.parse
+(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+
+// Import in to db
+
+const importData = async()=>{
+    try{
+       await Bootcamp.create(bootcamps)
+       console.log('Data importe...'.green.inverse)
+       process.exit();
+    }catch(err){
+       console.error(err)
+    }
+}
+
+
+// delete data
+
+const deleteData = async()=>{
+    try{
+       await Bootcamp.deleteMany()
+       console.log('Data destroyed...'.red.inverse)
+       process.exit();
+    }catch(err){
+       console.error(err)
+    }
+}
+
+if(process.argv[2]=== '-i'){
+    importData()
+}
+else if (process.argv[2]=== '-d'){
+    deleteData()
+}
